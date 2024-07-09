da14ad24f94bd8de05011fd2a2db3acd6cc47e64
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-07-09 15:55:03 +0530

    add auto-login

--- src/components/shared/AutoLogin.tsx
+++ src/components/shared/AutoLogin.tsx
@@ -1,12 +1,11 @@
 import { useSignInAccount } from "@/lib/react-query/queries";
 import  { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Loader from "./Loader";
 import { useUserContext } from "@/context/AuthContext";
 const AutoLogin = () => {
   const navigate = useNavigate();
-  const { mutateAsync: signIn, isLoading } = useSignInAccount();
-  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const { mutateAsync: signIn } = useSignInAccount();
+  const { checkAuthUser } = useUserContext();
   useEffect(() => {
     const loginAccount = async () => {
       await signIn({ email: "parmarharsh91065@gmail.com", password: "12345678" });
